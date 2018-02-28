1c3d99ee0809bc5a24facd94a4b6f0d17af7bfad
kiritheka <kiritheka@5gindia.net>
2018-02-28 17:34:54 +0530

    refactored code

--- newe/Portfolio.java
+++
@@ -1,6 +0,0 @@
-package newe;
-
-public interface Portfolio {
-	
-	void stockValue (int GOOG,int INFY,int MS,int AMZN);
-	}
---
+++ refactor/FileOperation.java
@@ -0,0 +1,10 @@
+package refactor;
+
+import java.io.IOException;
+
+public interface FileOperation {
+
+	String[] FileReader() throws IOException;
+
+	void FileWriter();
+}
